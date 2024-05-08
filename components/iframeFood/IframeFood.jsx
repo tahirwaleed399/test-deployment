3a366ac5032180e5ae055d57a661255f1e7e6393
Waleed Tahir <156667045+waleedtahir399@users.noreply.github.com>
2024-05-08 16:07:22 +0500

    some fix 2

--- components/iframeFood/IframeFood.jsx
+++ components/iframeFood/IframeFood.jsx
@@ -43,7 +43,7 @@ const IframeFood = () => {
 
         <iframe
   id="food-iframe"
-  src={iframeSrc}
+  src={`${window.origin}/foodGallery/FoodGallery.htm`}
   scrolling="yes"
   frameBorder="0"
   allowFullScreen={true}
